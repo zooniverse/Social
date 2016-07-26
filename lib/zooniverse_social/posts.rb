fdcd933383c8c2f2e213f76eded7230fbf696ead
Michael Parrish <michael@zooniverse.org>
2016-07-26 12:29:45 -0500

    Add daily blog posts.  Closes #1

--- lib/zooniverse_social/posts.rb
+++ lib/zooniverse_social/posts.rb
@@ -5,13 +5,20 @@ module ZooniverseSocial
     attr_reader :data
 
     def initialize
-      @updater = Updater.new 'https://public-api.wordpress.com', '/rest/v1.1/sites/36711287/posts'
+      @blog_updater = Updater.new 'https://public-api.wordpress.com', '/rest/v1.1/sites/36711287/posts'
+      @daily_updater = Updater.new 'https://public-api.wordpress.com', '/rest/v1.1/sites/57182749/posts'
       update
     end
 
     def update
-      response = @updater.update number: 3, fields: 'ID,URL,title,date'
-      @data = response.fetch('posts', []).collect do |post|
+      blog_data = _update @blog_updater
+      daily_data = _update @daily_updater
+      @data = (blog_data + daily_data).sort{ |a, b| b[:created_at] <=> a[:created_at] }.take 3
+    end
+
+    def _update(updater)
+      response = updater.update number: 3, fields: 'ID,URL,title,date'
+      response.fetch('posts', []).collect do |post|
         {
           id: post['ID'],
           title: post['title'],
--- spec/zooniverse_social/posts_spec.rb
+++ spec/zooniverse_social/posts_spec.rb
@@ -1,49 +1,79 @@
 module ZooniverseSocial
   RSpec.describe Posts do
+    before(:each) do
+      allow(Updater).to receive(:new).with(
+        'https://public-api.wordpress.com', '/rest/v1.1/sites/36711287/posts'
+      ).and_return blog_updater
+
+      expect(Updater).to receive(:new).with(
+        'https://public-api.wordpress.com', '/rest/v1.1/sites/57182749/posts'
+      ).and_return daily_updater
+    end
+
+    let(:blog_updater){ double update: blog_response }
+    let(:daily_updater){ double update: daily_response }
+
+    let(:blog_response){ { } }
+    let(:daily_response){ { } }
+
     describe '#initialize' do
       it 'should use an Updater' do
-        expect(Updater).to receive(:new).with(
-          'https://public-api.wordpress.com', '/rest/v1.1/sites/36711287/posts'
-        ).and_return double update: { }
+        expect(subject.instance_variable_get(:@blog_updater)).to eql blog_updater
+        expect(subject.instance_variable_get(:@daily_updater)).to eql daily_updater
         subject
       end
 
       it 'should update' do
-        allow_any_instance_of(Posts).to receive :update
-        expect(subject).to have_received :update
+        subject
+        expect(blog_updater).to have_received :update
+        expect(daily_updater).to have_received :update
       end
     end
 
     describe '#update' do
-      let(:sample_response) do
+      let(:blog_response) do
         {
           'posts' => [{
             'ID' => 123,
-            'title' => 'title',
-            'date' => 'now',
-            'URL' => 'url'
+            'title' => 'title1',
+            'date' => '1',
+            'URL' => 'url1'
           }]
         }
       end
 
-      it 'should call the updater' do
-        expect_any_instance_of(Updater).to receive(:update)
-          .at_least(:once)
-          .with(number: 3, fields: 'ID,URL,title,date')
-          .and_return sample_response
+      let(:daily_response) do
+        {
+          'posts' => [{
+            'ID' => 456,
+            'title' => 'title2',
+            'date' => '2',
+            'URL' => 'url2'
+          }]
+        }
+      end
+
+      it 'should call the updaters' do
+        [blog_updater, daily_updater].each do |updater|
+          expect(updater).to receive(:update)
+            .at_least(:once)
+            .with(number: 3, fields: 'ID,URL,title,date')
+        end
         subject.update
       end
 
       it 'should format the data' do
-        expect_any_instance_of(Updater).to receive(:update)
-          .at_least(:once)
-          .and_return sample_response
         subject.update
         expect(subject.data).to eql [{
+          id: 456,
+          title: 'title2',
+          created_at: '2',
+          link: 'url2'
+        }, {
           id: 123,
-          title: 'title',
-          created_at: 'now',
-          link: 'url'
+          title: 'title1',
+          created_at: '1',
+          link: 'url1'
         }]
       end
     end
